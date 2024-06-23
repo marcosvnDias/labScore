95d7df1c66f968330e7217f72af8979874709693
Marcos Santos <mvdias1268@gmail.com>
2024-06-23 02:23:26 -0300

    Resolução dos cards

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-let nomeDados = prompt("Qual o nome do aluno?");
-let idadeDados = prompt("Qual a idade do aluno?");
-let serieDados = prompt("Qual a série do aluno?");
-let nomeEscolaDados = prompt("Qual o nome da escola?");
-let materiaFavoritaDados = prompt("Qual a sua matéria favorita?");
+// let nomeDados = prompt("Qual o nome do aluno?");
+// let idadeDados = prompt("Qual a idade do aluno?");
+// let serieDados = prompt("Qual a série do aluno?");
+// let nomeEscolaDados = prompt("Qual o nome da escola?");
+// let materiaFavoritaDados = prompt("Qual a sua matéria favorita?");
 
-let nomeId = document.getElementById("nome")
-let idadeId = document.getElementById("idade")
-let serieId = document.getElementById("serie")
-let escolaId = document.getElementById("escola")
-let materiaFavoritaId = document.getElementById("materiaFavorita") 
+// let nomeId = document.getElementById("nome")
+// let idadeId = document.getElementById("idade")
+// let serieId = document.getElementById("serie")
+// let escolaId = document.getElementById("escola")
+// let materiaFavoritaId = document.getElementById("materiaFavorita") 
 
-function pegarDadosDeAluno() {
-nomeId.innerHTML = `<strong>Nome: </strong> ${nomeDados}`
-idadeId.innerHTML = `<strong>Idade: </strong> ${idadeDados}`
-serieId.innerHTML = `<strong>Série: </strong> ${serieDados}`
-escolaId.innerHTML = `<strong>Escola: </strong> ${nomeEscolaDados}`
-materiaFavoritaId.innerHTML = `<strong>Matéria Favorita: </strong> ${materiaFavoritaDados}`
-}
+// function pegarDadosDeAluno() {
+// nomeId.innerHTML = `<strong>Nome: </strong> ${nomeDados}`
+// idadeId.innerHTML = `<strong>Idade: </strong> ${idadeDados}`
+// serieId.innerHTML = `<strong>Série: </strong> ${serieDados}`
+// escolaId.innerHTML = `<strong>Escola: </strong> ${nomeEscolaDados}`
+// materiaFavoritaId.innerHTML = `<strong>Matéria Favorita: </strong> ${materiaFavoritaDados}`
+// }
 
-pegarDadosDeAluno()
+// pegarDadosDeAluno()
 
 function novaLinha() {
   let curso = pegarMateriaEnotas();
@@ -60,16 +60,22 @@ function pegarMateriaEnotas() {
 
   if (arrayNotas.length === 4) {
     let mediaNotas = fazerMedia(arrayNotas);
-    arrayMediasMaterias.push(mediaNotas);
-
     let curso = {
       materia: nomeMateria,
       notas: arrayNotas,
       media: mediaNotas
     };
 
-    mediasGerais.push(curso.media);
-    
+    let notasAlunoString = localStorage.getItem('notasAluno');
+    let notasAluno = JSON.parse(notasAlunoString);
+
+    if (!notasAluno[0].materia) {
+      notasAluno = [curso];
+    } else {
+      notasAluno.push(curso);
+    }
+
+    localStorage.setItem('notasAluno', JSON.stringify(notasAluno));
     return curso;
 
   } else {
@@ -100,9 +106,17 @@ function criarLinha(curso) {
 
   tbody.innerHTML += linha;
 
+  let notasAlunoString = localStorage.getItem('notasAluno');
+  let notasAluno = JSON.parse(notasAlunoString);
 
-  let geralMediaGeral = calcMediaGeral(mediasGerais);
-  let geralMaiorNota = ordenar(arrayMediasMaterias);
+  console.log(notasAluno)
+
+  let medias = notasAluno.map((curso) => {
+    return curso.media;
+  });
+
+  let geralMediaGeral = calcMediaGeral(medias);
+  let geralMaiorNota = ordenar(medias);
 
   insereMediaGeral(geralMediaGeral);
   insereMaiorMedia(geralMaiorNota);
@@ -130,10 +144,145 @@ function ordenar(arrayNumeros) {
   let numeroMaior = 0;
   for (let i = 0; i < arrayNumeros.length; i++) {
     let isNumeroFinal = i >= arrayNumeros.length ? true : false;
-    debugger;
+
     if (arrayNumeros[i] > numeroMaior && !isNumeroFinal) {
       numeroMaior = arrayNumeros[i];
     }
   }
   return numeroMaior;
+}
+
+async function inserirEnderecoCampo() {
+  let cepDados = document.getElementById("cepDados");
+  let ruaDados = document.getElementById("ruaDados");
+  let cidadeDados = document.getElementById("cidadeDados");
+  let estadoDados = document.getElementById("estadoDados");
+  let cep = cepDados.value;
+
+  if (cep.length >= 8) {
+    let data = fazerFetchCep(cep);
+
+    data
+    .then((info) => {
+      let endereco = info;
+
+      if (endereco.uf) {
+        cepDados.style.borderBottom = "2px solid black";
+        ruaDados.value = endereco.logradouro;
+        cidadeDados.value = endereco.localidade;
+        estadoDados.value = endereco.uf;
+      }
+    })
+    .catch((err) => {
+      cepDados.style.borderBottom = "2px solid #c90808";
+      console.error(err);
+    })
+  }
+}
+
+async function inserirAlunosCampo(data) {
+  let lista = document.querySelectorAll("#lista-alunos li");
+  console.log(data)
+
+  data
+  .then((info) => {
+    lista.forEach((li, index) => {
+      li.textContent = info[index].nome
+      console.log(info.nome)
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+}
+
+async function fazerFetchCep(cepDados) {
+  let url = "https://viacep.com.br/ws/" + cepDados + "/json/";
+  const request = await fetch(url, {method: "GET"});
+  return await request.json();
+}
+
+async function fazerFetchAlunos() {
+  let url = "./alunos.json";
+  const request = await fetch(url, {method: "GET"});
+  return await request.json();
+}
+
+function pegarDadosAluno() {
+  let nomeDados = document.getElementById("nomeDados");
+  let idadeDados = document.getElementById("idadeDados");
+  let serieDados = document.getElementById("serieDados");
+  let escolaDados = document.getElementById("escolaDados");
+  let materiaFavoritaDados = document.getElementById("materiaDados");
+  let cepDados = document.getElementById("cepDados");
+  let ruaDados = document.getElementById("ruaDados");
+  let cidadeDados = document.getElementById("cidadeDados");
+  let estadoDados = document.getElementById("estadoDados");
+
+  let dadosAluno = {
+    nome: nomeDados.value,
+    idade: idadeDados.value,
+    serie: serieDados.value,
+    nomeEscola: escolaDados.value,
+    materiaFavorita: materiaFavoritaDados.value,
+    cep: cepDados.value,
+    rua: ruaDados.value,
+    cidade: cidadeDados.value,
+    estado: estadoDados.value
+  }
+
+  if (ruaDados.value) {
+    let notasAluno = [{
+      materia: "",
+      notas: [0, 0, 0, 0],
+      media: 0
+    }];
+    localStorage.setItem('dadosAluno', JSON.stringify(dadosAluno));
+    localStorage.setItem('notasAluno', JSON.stringify(notasAluno));
+
+    console.log(localStorage.getItem('dadosAluno'))
+    location.assign("./index.html");
+  }
+
+}
+
+function inserirDadosAlunoCampo() {
+  let nomeId = document.getElementById("nome");
+  let idadeId = document.getElementById("idade");
+  let serieId = document.getElementById("serie");
+  let escolaId = document.getElementById("escola");
+  let materiaFavoritaId = document.getElementById("materiaFavorita");
+  let cepId = document.getElementById("cepId");
+  let ruaId = document.getElementById("ruaId");
+  let cidadeId = document.getElementById("cidadeId");
+  let estadoId = document.getElementById("estadoId");
+
+  let dadosAlunoString = localStorage.getItem('dadosAluno');
+  let dadosAluno = JSON.parse(dadosAlunoString);
+
+  let notasAlunoString = localStorage.getItem('notasAluno');
+  let notasAluno = JSON.parse(notasAlunoString);
+
+  let medias = notasAluno.map((curso) => {
+    return curso.media;
+  });
+
+  let geralMediaGeral = calcMediaGeral(medias);
+  let geralMaiorNota = ordenar(medias);
+
+  insereMediaGeral(geralMediaGeral);
+  insereMaiorMedia(geralMaiorNota);
+
+  let data = fazerFetchAlunos();
+  inserirAlunosCampo(data);
+
+  nomeId.innerHTML = `<strong>Nome: </strong> ${dadosAluno.nome}`;
+  idadeId.innerHTML = `<strong>Idade: </strong> ${dadosAluno.idade}`;
+  serieId.innerHTML = `<strong>Série: </strong> ${dadosAluno.serie}`;
+  escolaId.innerHTML = `<strong>Escola: </strong> ${dadosAluno.nomeEscola}`;
+  materiaFavoritaId.innerHTML = `<strong>Matéria Favorita: </strong> ${dadosAluno.materiaFavorita}`;
+  cepId.innerHTML = `<strong>CEP: </strong> ${dadosAluno.cep}`;
+  ruaId.innerHTML = `<strong>Rua: </strong> ${dadosAluno.rua}`;
+  cidadeId.innerHTML = `<strong>Cidade: </strong> ${dadosAluno.cidade}`;
+  estadoId.innerHTML = `<strong>Estado: </strong> ${dadosAluno.estado}`;
 }
